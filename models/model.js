eb53632e6025b365092f1d78404bd1fb36ea8aa2
ah98607 <adh6107@gmail.com>
2017-07-26 19:30:52 -0700

    template completed

---
+++ models/model.js
@@ -0,0 +1,68 @@
+// import npm module
+var Sequelize = require("sequelize");
+
+// configure sequelize instance
+// be sure to run mysql command to create database instance
+var sequelize = new Sequelize("database_name", "root", "andyhemysql123", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+// define table structure
+var myTable = sequelize.define("table_name",
+{
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  }
+},
+{
+  timestamps: false
+});
+
+// sync table
+myTable.sync();
+
+// combined model and ORM
+var model = {
+  all: function(callback) {
+    myTable.findAll(
+    {
+      // empty constraint
+    }).then(function(result) {
+      callback(result);
+    });
+  },
+  search: function(callback) {
+    myTable.findAll(
+      {
+        where:
+        {
+          name: name
+        }
+      }).then(function(result) {
+        callback(result);
+      });
+    },
+    add: function(callback) {
+      myTable.create(
+      {
+        id: 1,
+        name: character.name
+      }).then(function(result) {
+        callback(result);
+      });
+    }
+  };
+
+// export model to controller
+module.exports = model;
